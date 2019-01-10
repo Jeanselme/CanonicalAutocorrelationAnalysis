11a688cf74910c4f0bb9b927a67d0c2412cf7e07
Vincent <vincent.jeanselme@gmail.com>
2019-01-10 16:13:53 -0500

    From polar to Hough

--- model/hough.py
+++ model/hough.py
@@ -4,24 +4,38 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def hough(projection, points, discretization = 100):
+def hough(projection, points, discretizationRadius = 100, discretizationAngle = 180):
     """
         Computes hough transforms of the points
     
         Arguments:
             projection {Projection} -- Object obtained from CAA
-            points -- Datapoints to project and anlyze 
+            points -- Datapoints to project and analyze 
+
+            discretizationRadius {Int} 
+            discretizationAngle {Int}
+        
     """
     # Coordinates in the projection space
     projected = projection.projectPoints(points)
 
-    # Coordinate in the Hough space
+    # Polar coordinate of all points
     r = np.sqrt(projected[:,0]**2 + projected[:,1]**2)
     theta = np.arctan2(projected[:,1], projected[:,0])
 
-    return np.column_stack([r, theta * 180 / np.pi])
+    # Hough space
+    houghSpace = np.zeros((discretizationRadius, discretizationAngle))
+    radiusBins = np.linspace(0, np.max(r), discretizationRadius + 1)
+    angleBins = np.linspace(0, np.pi, discretizationAngle)
+    for i, phi in enumerate(angleBins):
+        # For the given theta compute the radius of the line cutting the points
+        radPhi = np.abs(np.cos(phi - theta) * r)
+        hist, _ = np.histogram(radPhi, bins=radiusBins)
+        houghSpace[:, i] += hist
 
-def displayHough(projection, points, plt, color):
+    return np.flip(houghSpace, 0), angleBins * 180 / np.pi, radiusBins
+
+def displayHough(projection, points):
     """
         Computes hough transforms of the points and displays it
         Call this multiple times to display different serieses in different colors
@@ -32,8 +46,10 @@ def displayHough(projection, points, plt, color):
             plt -- Plot to display points. 
             color -- Color name for plotting 'points' 
     """
-    hough_coordinate = hough(projection, points)
+    houghSpace, xaxis, yaxis = hough(projection, points)
 
-    plt.scatter(hough_coordinate[:,1], hough_coordinate[:,0], facecolors='none', edgecolors=color, alpha=0.3)
+    plt.figure()
+    plt.imshow(houghSpace, extent=[xaxis[0], xaxis[-1], yaxis[0], yaxis[-1]], aspect='auto', cmap='gray')
     plt.ylabel("Radius")
     plt.xlabel("Angle(in deg)")
+    plt.show()
